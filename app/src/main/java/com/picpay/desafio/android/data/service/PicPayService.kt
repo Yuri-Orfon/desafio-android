2554ad289ba818cea172ea10eab86105be510dca
Yuri Orfon <yurigoncalves1964@hotmail.com>
2021-08-29 21:21:08 -0300

    extract base url and adding provideRetrofit

--- app/src/main/java/com/picpay/desafio/android/data/service/PicPayService.kt
+++ app/src/main/java/com/picpay/desafio/android/data/service/PicPayService.kt
@@ -6,6 +6,10 @@ import retrofit2.http.GET
 
 interface PicPayService {
 
+    companion object {
+        const val BASE_URL = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+    }
+
     @GET("users")
     fun getUsers(): Call<List<User>>
 }
--- app/src/main/java/com/picpay/desafio/android/di/RetrofitModule.kt
+++ app/src/main/java/com/picpay/desafio/android/di/RetrofitModule.kt
@@ -1,12 +1,10 @@
 package com.picpay.desafio.android.di
 
-import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.data.service.PicPayService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -17,14 +15,15 @@ object RetrofitModule {
 
     @Singleton
     @Provides
-    fun providePicPayService(): PicPayService {
-        return Retrofit.Builder()
-            .baseUrl("https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/")
-            .client(
-                OkHttpClient.Builder()
-                .build())
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(PicPayService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(PicPayService::class.java)
-    }
+
+    @Singleton
+    @Provides
+    fun providePicPayService(retrofit: Retrofit): PicPayService =
+         retrofit.create(PicPayService::class.java)
+
 }
