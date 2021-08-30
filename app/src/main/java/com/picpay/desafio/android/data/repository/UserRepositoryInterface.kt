2bce3bd54f89d99ee7d82b635771847b228c5cc6
Yuri Orfon <yurigoncalves1964@hotmail.com>
2021-08-30 17:16:32 -0300

    removing dataModule and dataSource

--- app/src/main/java/com/picpay/desafio/android/data/repository/UserRepositoryInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.picpay.desafio.android.data.repository
-
-import com.picpay.desafio.android.data.model.User
-import retrofit2.Call
-
-interface UserRepositoryInterface {
-    fun getUsers(): Call<List<User>>
-}
